bb5ef817ae274b3e9717183b554d3f4af53622cd
arthurcartana <arthurcmdossantos@gmailcom>
2026-08-28 21:38:18 -0300

    feat: adicionando dtos

--- backend/src/routes/GameRoutes.ts
+++ backend/src/routes/GameRoutes.ts
@@ -4,6 +4,8 @@ import { GameController } from "../controllers/GameController";
 const gameRoutes = Router()
 const controller = new GameController()
 
-gameRoutes.post('/create', controller.register.bind(controller))
+gameRoutes.post('/create', controller.create.bind(controller))
+gameRoutes.patch('/games/:id', controller.update.bind(controller))
+gameRoutes.delete('/games/:id', controller.delete.bind(controller))
 
 export default gameRoutes
